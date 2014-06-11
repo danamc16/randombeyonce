d56e89064856df0210705df5c650f5586ec525df
Dana Chaykovsky <dana.chaykovsky@yale.edu>
2014-06-11 10:50:24 -0400

    ALL THE THINGS

---
+++ randombeyonce.rb
@@ -0,0 +1,75 @@
+require 'sinatra'
+
+configure do
+	enable :sessions
+end
+
+# sets => {
+# 	"SETNAME" => {
+# 		"name" => "SETNAME", "vidnums" =>
+# 		["lLJf9qJHR3E","RB-RcX5DS5A"]
+# 	}
+# }
+
+videos = ["lLJf9qJHR3E","RB-RcX5DS5A"]
+set = Hash.new{}
+
+get '/' do
+	session[:history] ||= []
+	result = videos[rand(0...videos.length)]
+
+	erb :index, :locals => {:result => result}
+
+end
+
+get '/sets' do
+	session[:history] ||=[]
+	params[:setname]
+ 	params[:vids]
+	params[:description]
+
+	erb :sets, :locals => {:setname => params[:setname]
+						   :vids => params[:vids],
+						   :description => params[:description]
+							}
+end
+
+post '/sets' do
+	session[:history] ||= []
+	vidlist = params[:vids].split(", ")
+	if params[:submit] == "submit"
+		session[:history].store(params[:setname] => vidlist.push(params[:description])
+			rand(0..vidlist.length-1)
+	end
+end
+
+
+# get '/sets/previous' do
+# 	result_name = session[:setname]
+# 	result_vids = session[:vids]
+# 	result_description = session[:description]
+
+
+# 	erb :previous, :locals => {:result_name => result_name,
+# 								:result_vids => result_vids,
+# 								:result_description => result_description}
+# end
+
+
+# post '/sets' do
+
+# 	sets[] = params['SETNAME']
+
+# end
+
+
+# HTTP Verb | URL | Controller | Action | used for | Must create View?
+# --- | --- | --- | --- | --- | ---
+# GET | /sets | Set | index | display all sets in an overview | Yes
+# GET | /sets/new | Set | new | return an HTML form for creating a new set | Yes
+# POST | /sets | Set | create | create a new set | No
+# GET | /sets/beyonce | Set | show | display a specific set | Yes
+# GET | /sets/beyonce/play | Set | a custom one! | play a specific set | Yes
+# GET | /sets/beyonce/edit | Set | edit | return an HTML form for editing a set | Yes
+# PUT | /sets/beyonce | Set | update | update a specific set | No
+# DELETE | /sets/beyonce | Set | destroy | delete a specific set | Yes
