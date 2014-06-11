d4e385f937eaae25077ec027a56b3aa752f63677
CoffeeCakeCat <yeeruhuang@gmail.com>
2014-06-11 13:36:37 -0400

    fixed errors and hard-coded Arctic Monkeys

--- randombeyonce.rb
+++ randombeyonce.rb
@@ -12,10 +12,6 @@ end
 # 	}
 # }
 
-set = Hash.new{}
-
-
-
 # get '/sets' do
 # 	session[:history] ||=[]
 # 	params[:setname]
@@ -26,7 +22,7 @@ set = Hash.new{}
 # end
 
 get '/' do
-	session[:history] ||= []
+	session[:history] ||= {}
 	erb :index, :locals => {:history => session[:history]}
 end
 
@@ -45,9 +41,9 @@ post '/sets' do
 end
 
 get '/sets' do
-	session[:history] ||= []
+	session[:history] ||= {}
 
-	erb :"index.html", :locals => {:history => session[:history]}
+	erb :sets, :locals => {:history => session[:history]}
 end
 
 # get '/sets/previous' do
